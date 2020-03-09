c18b276541efb5d93fe9299310c1b5eff66584dc
Swaleha Saleem <sualehasaleem12@gmail.com>
2020-03-09 13:34:22 +0500

    final

--- graphs.py
+++ graphs.py
@@ -3,11 +3,9 @@ class Edge:
 
     def __init__(self, v0: int, v1: int):
         """Create edge with endpoints at v0 and v1.
-
         Args:
         - self: the instance to create.
         - v0, v1: endpoints of the edge
-
         Returns:
         nothing.
         """
@@ -15,26 +13,20 @@ class Edge:
 
     def __repr__(self) -> str:
         """Returns string representation of edge for printing.
-
         Allows `print()` on instances.
-
         Args:
         - self: this instance.
-
         Returns:
         string representation for printing.
         """
         return f'({self.v0}, {self.v1})'
 
-    def __eq__(self, other: Edge) -> bool:
+    def __eq__(self, other) -> bool:
         """Does other have the same enpoints?
-
         Allows `==` on instances.
-
         Args:
         - self: this instance.
         - other: edge to compare with.
-
         Returns:
         True if other has the same endpoints as this edge.
         """
@@ -44,28 +36,21 @@ class Edge:
 
     def __hash__(self) -> int:
         """Returns hash of this edge.
-
         Makes instances hashable, allows adding them to appropraite containers,
         e.g. `set`, `dict`.
-
         Args:
         - self: this instance.
-
         Returns:
         a hash of this edge.
-
         """
         return hash(tuple(sorted(self.__dict__.items())))
 
     def __contains__(self, v) -> bool:
         """Is v an endpoint of this edge?
-
         Allows `in` syntax.
-
         Args:
         - self: this instance.
         - v: the vertex to check.
-
         Returns:
         True if v is an endpoint, False otherwise.
         """
@@ -73,11 +58,9 @@ class Edge:
 
     def nbr(self, v: int) -> int:
         """Returns the neighbor of v along this edge if v is an endpoint.
-
         Args:
         - self: this instance.
         - v: the vertex whose neighbor is sought.
-
         Returns:
         the other endpoint if v is an endpoint, otherwise None.
         """
@@ -89,146 +72,365 @@ class Graph:
 
     def __init__(self, edges: str, imp: str):
         """Creates graph with the given edges using the specified implementation.
-
         edges consists of multiple lines representing an edge list
         representation of the graph. Each line contains 2 vertices and an
         optional weight. All values in a line are separated by spaces. The vertices have integer values
         and the optional weight is a float. The vertices need not begin at 0.
-
         the value of imp sepcifies the graph implementation to be used as follows:
         sets   : two sets, one each for the vertices and the edges
         matrix : adjacenccy matrix
         list   : adjacency list
-
         Args:
         self: the instance to create.
         edges: an edge list representation of the graph
         imp: the implementation to be used
-
         Returns:
         nothing.
         """
         if imp == "sets":
             self.graph = SetGraph(edges)
-        elif imp == "matrix":
+        if imp == "matrix":
             self.graph = AdjacencyMatrix(edges)
         elif imp == "list":
             self.graph = AdjacencyList(edges)
 
     def vertices(self):
         """Iterates over the vertices in the graph.
-
         Args:
         - self: the instance to operate on.
-
         Returns:
         nothing.
-
         Yields:
         vertices in the graph.
         """
-        pass
+        return self.graph.vertices()
 
     def edges(self) -> {Edge}:
         """Iterates over the edges in the graph.
-
         Args:
         - self: the instance to operate on.
-
         Returns:
         nothing.
-
         Yields:
         vertices in the graph.
         """
-        pass
+        return self.graph.edges()
 
     def vertex_count(self) -> int:
         """Returns the number of vertices in the graph.
-
         Args:
         - self: the instance to operate on.
-
         Returns:
         the number of vertices in the graph.
         """
-        pass
+        return self.graph.vertex_count()
 
     def edge_count(self) -> int:
         """Returns the number of edges in the graph.
-
         Args:
         - self: the instance to operate on.
-
         Returns:
         the number of edges in the graph.
         """
-        pass
+        return self.graph.edge_count()
 
     def has_vertex(self, v) -> bool:
         """Returns whether v is a vertex in the graph.
-
         Args:
         - self: the instance to operate on.
         - v: its neighbors in the graph are to be returned.
-
         Returns:
         True if v is a vertex in the graph, False otherwise.
         """
-        pass
+        return self.graph.has_vertex(v)
 
     def has_edge(self, v0, v1) -> bool:
         """Returns whether the grpah contains an edge between v0 and v1.
-
         Args:
         - self: the instance to operate on.
         - v0, v1: does an edge exist between vertices v0 and v1 in the graph?
-
         Returns:
         True if an edge exists between v0 and v1 in the graph, False otherwise.
         """
         assert self.has_vertex(v0) and self.has_vertex(v1), \
             f'one or more of {v0} and {v1} are not valid vertices'
-        pass
+        return self.graph.has_edge(v0, v1)
 
     def has_weights(self) -> bool:
         """Returns whether the graph is weighted.
-
         Args:
         - self: the instance to operate on.
-
         Returns:
         True if the graph edges are weighted, False otherwise.
         """
-        pass
+        return self.graph.has_weights()
 
     def neighbors(self, v):
         """Iterates over the neighbors of the vertex v in the graph.
-
         Errors if v is not in the graph. Check before calling.
-
         Args:
         - self: the instance to operate on.
         - v: the vertex whose neighbors in the graph are sought.
-
         Returns:
         nothing.
-
         Yields:
         neighbors of v in the graph.
         """
-        pass
+        return self.graph.neighbors(v)
 
     def degree(self, v) -> {int}:
         """Returns the degree of the vertex v in the graph.
-
         Errors if v is not in the graph. Check before calling.
-
         Args:
         - self: the instance to operate on.
         - v: its degree in the graph is to be returned.
-
         Returns:
         degree of v in the graph.
         """
-        pass
+        return self.graph.degree(v)
+
+    def weight(self, v0: int, v1: int):
+        """Returns the weight of the edge between v0 and v1; None if no weight.
+        Assumes the presence of the edge between v0 and v1. Check before calling.
+        Args:
+        - self: the instance to operate on.
+        - v0, v1: the weight of the edge between v0 and v1 is sought.
+        Returns:
+        The weight of the edge between v0 and v1; None if graph is unweighted.
+        """
+        return self.graph.weight(v0, v1)
+
+
+""" Set Graph """
+
+
+class SetGraph(Graph):
+    def __init__(self, edges):
+        self.verset = set([])
+        self.edgeset = set([])
+        self.weighted = False
+        self.edgeCount = 0
+        i = 0
+        for line in edges.splitlines():
+            line = line.split()
+            if len(line) > 2:
+                line = [int(line[0]), int(line[1]), eval(line[2])]
+                self.edgeset.add((Edge(line[0], line[1]), line[2]))
+            else:
+                line = [int(line[0]), int(line[1])]
+                self.edgeset.add((Edge(line[0], line[1]), 1))
+            for ver in line:
+                if ver not in self.verset:
+                    i += 1
+                    self.verset.add(ver)
+            self.edgeCount += 1
+        self.verCount = i
+        if len(line) > 2:
+            self.weighted = True
+
+    def vertices(self):
+        for ver in self.verset:
+            yield ver
+
+    def edges(self):
+        for edge in self.edgeset:
+            yield edge[0]
+
+    def vertex_count(self) -> int:
+        return self.verCount
+
+    def edge_count(self) -> int:
+        return self.edgeCount
+
+    def has_vertex(self, v) -> bool:
+        return v in self.verset
+
+    def has_edge(self, v0, v1) -> bool:
+        for i in self.edgeset:
+            if v0 in i[0] and i[0].nbr(v0) == v1:
+                return True
+        return False
+
+    def has_weights(self) -> bool:
+        return self.weighted
+
+    def neighbors(self, v):
+        for edge in self.edgeset:
+            if v in edge[0]:
+                yield edge[0].nbr(v)
+
+    def degree(self, v) -> {int}:
+        deg = 0
+        for edge in self.edgeset:
+            if v in edge[0]:
+                deg += 1
+        return deg
+
+    def weight(self, v0: int, v1: int):
+        if self.weighted and self.has_edge(v0, v1):
+            for i in self.edgeset:
+                if v0 in i[0] and i[0].nbr(v0) == v1:
+                    return i[1]
+        return None
+
+
+class AdjacencyMatrix:
+    def __init__(self, edges):
+        self.verdict = dict()
+        self.weighted = False
+        self.matrix = []
+        i = 0  # mapping counter for vertices/number of vertices
+        self.edgeCount = 0
+        lst = []
+        edges = edges.splitlines()
+        for line in edges:
+            line = line.split()
+            line = [int(line[0]), int(line[1])]
+            for ver in line:
+                if ver not in self.verdict:
+                    self.verdict[ver] = i
+                    i += 1
+                    lst.append(0)
+            self.edgeCount += 1  # number of times the for loop runs the number of edges
+        self.verCount = i
+        for i in range(self.verCount):
+            self.matrix.append(list(lst))
+
+        for line in edges:
+            line = line.split()
+            if len(line) > 2:
+                line = [int(line[0]), int(line[1]), eval(line[2])]
+                self.matrix[self.verdict[line[0]]
+                            ][self.verdict[line[1]]] = line[2]
+                self.matrix[self.verdict[line[1]]
+                            ][self.verdict[line[0]]] = line[2]
+            else:
+                line = [int(line[0]), int(line[1])]
+                self.matrix[self.verdict[line[0]]][self.verdict[line[1]]] = 1
+                self.matrix[self.verdict[line[1]]][self.verdict[line[0]]] = 1
+        if len(line) > 2:
+            self.weighted = True
+
+    def vertices(self):
+        for key in self.verdict.keys():
+            yield key
+
+    def edges(self):
+        keyList = list(self.verdict.keys())
+        valueList = list(self.verdict.values())
+        visited = []
+        for i in range(self.verCount):
+            ver1 = keyList[valueList.index(i)]
+            for j in range(self.verCount):
+                ver2 = keyList[valueList.index(j)]
+                if self.matrix[i][j] and ver2 not in visited:
+                    yield Edge(ver1, ver2)
+            visited.append(ver1)
+
+    def vertex_count(self) -> int:
+        return self.verCount
+
+    def edge_count(self) -> int:
+        return self.edgeCount
+
+    def has_vertex(self, v) -> bool:
+        return v in self.verdict
+
+    def has_edge(self, v0, v1) -> bool:
+        return bool(self.matrix[self.verdict[v0]][self.verdict[v1]]) == True
+
+    def has_weights(self) -> bool:
+        return self.weighted
+
+    def neighbors(self, v):
+        keyList = list(self.verdict.keys())
+        valueList = list(self.verdict.values())
+        for i in range(self.verCount):
+            if self.matrix[self.verdict[v]][i]:
+                yield keyList[valueList.index(i)]
+
+    def degree(self, v) -> {int}:
+        deg = 0
+        for i in self.matrix[self.verdict[v]]:
+            if i:
+                deg += 1
+        return deg
+
+    def weight(self, v0: int, v1: int):
+        if self.has_edge(v0, v1) and self.weighted:
+            return self.matrix[self.verdict[v0]][self.verdict[v1]]
+        return None
+
+
+class AdjacencyList(Graph):
+
+    def __init__(self, edges):
+        self.adjList = dict()  # adjacency List representation
+        self.weighted = False  # weather the graph has weights or not
+        edj = 0
+        for line in edges.splitlines():
+            line = line.split()
+            if len(line) > 2:
+                line = [int(line[0]), int(line[1]), eval(line[2])]
+                self.adjList[line[0]] = self.adjList.get(
+                    line[0], []) + [(line[1], line[2])]
+                self.adjList[line[1]] = self.adjList.get(
+                    line[1], []) + [(line[0], line[2])]
+            else:
+                line = [int(line[0]), int(line[1])]
+                self.adjList[line[0]] = self.adjList.get(
+                    line[0], []) + [(line[1], 1)]
+                self.adjList[line[1]] = self.adjList.get(
+                    line[1], []) + [(line[0], 1)]
+            edj += 1
+        if len(line) > 2:
+            self.weighted = True
+        # number of keys represent the number of vertex
+        self.verCount = len(self.adjList)
+        self.edgeCount = edj
+
+    def vertex_count(self):
+        return self.verCount
+
+    def edge_count(self):
+        return self.edgeCount
+
+    def vertices(self) -> [int]:
+        for key in self.adjList.keys():
+            yield key
+
+    def edges(self):
+        visited = []
+        for key in self.adjList:
+            for val in self.adjList[key]:
+                if val[0] not in visited:
+                    yield Edge(key, val[0])
+            visited.append(key)
+
+    def has_vertex(self, v):
+        return v in self.adjList
+
+    def has_edge(self, v0, v1):
+        for val in self.adjList[v0]:
+            if val[0] == v1:
+                return True
+        return False
+
+    def degree(self, v):
+        if self.has_vertex(v):
+            return len(self.adjList[v])
+
+    def neighbors(self, v):
+        if self.has_vertex(v):
+            for ver in self.adjList[v]:
+                yield ver[0]
+
+    def has_weights(self):
+        return self.weighted
+
+    def weight(self, v0, v1):
+        if self.has_edge(v0, v1) and self.weighted:
+            for val in self.adjList[v0]:
+                if val[0] == v1:
+                    return val[1]
+        return None
--- networks.py
+++ networks.py
@@ -3,7 +3,7 @@ from graphs import *
 
 
 class NetworkOperations:
-    def degree_centrality(g: Graph, vtx: int) -> float:
+    def degree_centrality(self, g: Graph, vtx: int) -> float:
         """Returns the degree centrality of the vertex, vtx in the graph, g.
 
         Args:
@@ -13,9 +13,28 @@ class NetworkOperations:
         Returns:
         the degree centrality of vtx in g.
         """
-        pass
 
-    def clustering_coefficient(g: Graph, vtx: int = None) -> float:
+        return g.degree(vtx)/(g.vertex_count()-1)
+
+    def local_centrality(self, g: Graph, vtx: int) -> int:
+        nbr = list()
+        for neighbor in g.neighbors(vtx):
+            nbr.append(neighbor)
+        ki = len(nbr)
+        denominator = (ki*(ki-1))/2
+        L = 0
+        for i in nbr:
+            nv = g.neighbors(i)
+            for j in range(g.degree(i)):
+                if nv in nbr:
+                    L += 1
+                nv = next(g.neighbors(i))
+        if L:
+            return (L//2)/denominator
+        else:
+            return 0
+
+    def clustering_coefficient(self, g: Graph, vtx: int = None) -> float:
         """Returns the local or average clustering coefficient in g depending on vtx.
 
         vtx = None : average clustering coefficient of g
@@ -28,9 +47,16 @@ class NetworkOperations:
         Returns:
         the local or average clustering coefficient in g.
         """
-        pass
+        if vtx != None:
+            return self.local_centrality(g, vtx)
 
-    def average_neighbor_degree(g: Graph, vtx: int) -> float:
+        else:
+            sum = 0
+            for i in g.vertices():
+                sum += self.local_centrality(g, i)
+            return sum
+
+    def average_neighbor_degree(self, g: Graph, vtx: int) -> float:
         """Returns the average neighbor degree of vertex vtx in g.
 
         Args:
@@ -40,9 +66,16 @@ class NetworkOperations:
         Returns:
         the average neighbor degree of vtx in g.
         """
-        pass
+        nbr = list()
+        for neighbor in g.neighbors(vtx):
+            nbr.append(neighbor)
+        Ni = len(nbr)
+        sum = 0
+        for j in nbr:
+            sum += g.degree(j)
+        return sum/Ni
 
-    def similarity(g: Graph, v0: int, v1: int) -> float:
+    def similarity(self, g: Graph, v0: int, v1: int) -> float:
         """Returns the Jaccard similarity of vertices, v0 and v1, in g.
 
         Args:
@@ -52,9 +85,20 @@ class NetworkOperations:
         Returns:
         The Jaccard similarity of vertices, v0 and v1, in g.
         """
-        pass
+        nbr = list()
+        for neighbor in g.neighbors(v0):
+            nbr.append(neighbor)
+        nbr1 = list()
+        for neighbor in g.neighbors(v1):
+            nbr1.append(neighbor)
+        ni, nj = len(nbr), len(nbr1)
+        intersection = 0
+        for i in nbr:
+            if i in nbr1:
+                intersection += 1
+        return intersection/(ni+nj-intersection)
 
-    def popular_distance(g: Graph, vtx: int) -> int:
+    def popular_distance(self, g: Graph, vtx: int) -> int:
         """Returns the popular distance of the vertex, vtx, in g.
 
         Args:
@@ -64,9 +108,65 @@ class NetworkOperations:
         Returns:
         the popular distance of the vertex, vtx, in g.
         """
-        pass
+        # finding popular vertex first
+        source, max_dist = 0, 0
 
-    def visualize(g: Graph) -> None:
+        for i in g.vertices():
+            if g.degree(i) > max_dist:
+                source, max_dist = i, g.degree(i)
+
+        inf = float('inf')
+
+        # 1. Mark all nodes unvisited and store them.
+        # 2. Set the distance to zero for our initial node
+        # and to infinity for other nodes.
+        distances = {vertex: inf for vertex in g.vertices()}
+        previous_vertices = {
+            vertex: None for vertex in g.vertices()
+        }
+        distances[source] = 0
+        vertices = []
+        for i in g.vertices():
+            vertices.append(i)
+
+        while vertices:
+            # 3. Select the unvisited node with the smallest distance,
+            # it's current node now.
+            current_vertex = min(
+                vertices, key=lambda vertex: distances[vertex])
+
+            # 6. Stop, if the smallest distance
+            # among the unvisited nodes is infinity.
+            if distances[current_vertex] == inf:
+                break
+
+            # 4. Find unvisited neighbors for the current node
+            # and calculate their distances through the current node.
+            for neighbour in g.neighbors(current_vertex):
+                if g.weight(current_vertex, neighbour):
+                    alternative_route = distances[current_vertex] + \
+                        g.weight(current_vertex, neighbour)
+                else:
+                    alternative_route = distances[current_vertex]
+                # Compare the newly calculated distance to the assigned
+                # and save the smaller one.
+                if alternative_route < distances[neighbour]:
+                    distances[neighbour] = alternative_route
+                    previous_vertices[neighbour] = current_vertex
+
+            # 5. Mark the current node as visited
+            # and remove it from the unvisited set.
+            vertices.remove(current_vertex)
+
+        path, current_vertex = list(), vtx
+        while previous_vertices[current_vertex] is not None:
+            path.append(current_vertex)
+            current_vertex = previous_vertices[current_vertex]
+        if path:
+            path.insert(0, current_vertex)
+        return len(path)
+
+    def visualize(self, g: Graph) -> None:
         """Visualizes g.
 
         Args:
